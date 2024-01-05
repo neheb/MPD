f5c1759f55a53f6982c5182604fb0118d36690d0
Rosen Penev <rosenp@gmail.com>
2024-01-04 18:18:02 -0800

    m

--- src/net/SocketDescriptor.hxx
+++ src/net/SocketDescriptor.hxx
@@ -19,6 +19,10 @@
 #include <winsock2.h> // for SOCKET, INVALID_SOCKET
 #endif
 
+#ifdef HAVE_STRUCT_UCRED
+#include <sys/socket.h>
+#endif
+
 class SocketAddress;
 class StaticSocketAddress;
 class IPv4Address;
